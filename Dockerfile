b45e44f01c9a8a8d56325963fff7f871d216b876
Scott Rogers <scott@scottandrewrogers.com>
2025-02-24 16:08:40 -0500

    updated with pika example

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,14 @@
 FROM python:3.10-slim-bullseye 
 
-# FROM base AS builder
-
-# RUN apt update && apt install git python3-pip -y
-# RUN apt install bash -y
-# RUN apt autoremove
-
-# FROM base
-
 WORKDIR /mq-rcvr
 
-COPY mq-rcvr/requirements.txt /mq-rcvr/requirements.txt
+COPY src/requirements.txt /mq-rcvr/requirements.txt
 RUN pip3 install -r requirements.txt
 
 EXPOSE 5000
 
 ENV NEW_RELIC_APP_NAME=doodle-mq-rcvr
-# ENV OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
-# ENV OTEL_SERVICE_NAME=mq-rcvr
-# ENV OTEL_RESOURCE_ATTRIBUTES=service.instance.id=19-4-6
-# ENV OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
-# ENV OTEL_EXPORTER_OTLP_ENDPOINT=https://otlp.nr-data.net
-# ENV OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT=4095
-# ENV OTEL_EXPORTER_OTLP_COMPRESSION=gzip
-# ENV OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf 
-# ENV OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=delta
+ENV PYTHONUNBUFFERED=1
 
-# RUN opentelemetry-bootstrap -a install
-
-COPY mq-rcvr/src/ /mq-rcvr
-CMD newrelic-admin run-program flask run --host=0.0.0.0 -p 5000
-# CMD flask run --host=0.0.0.0 -p 5000
-# CMD opentelemetry-instrument --logs_exporter otlp flask run --debugger --host=0.0.0.0 -p 5000
+COPY src/app.py /mq-rcvr
+CMD python app.py
--- src/app.py
+++ src/app.py
@@ -1,73 +1,31 @@
-from flask import Flask
-import requests
-from flask import request
-import os
-from datetime import datetime
-from time import sleep
-from random import randrange
-import logging
+#!/usr/bin/env python
+import pika, sys, os
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.DEBUG)
+def main():
+    queue_name = os.environ.get('QUEUE', "hello")
+    rmq_host = os.environ.get('RMQ_HOST', "[HOST NOT SET")
 
-app = Flask(__name__)
-
-helloHost     = os.environ.get('HELLO_HOST', "localhost")
-worldHost     = os.environ.get('WORLD_HOST', "localhost")
-worldHostRuby = os.environ.get('WORLD_HOST_RUBY', "localhost")
-worldPort     = os.environ.get('WPORT', "5002")
-worldPortRuby = os.environ.get('WPORT_RUBY', "5002")
-shard         = os.environ.get('SHARD', "na")
-rubyWorld    = os.environ.get('RUBY_WORLD', "50")
-
-def logit(message):
-    timeString = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-    log.info(timeString + " - [mq-rcvr: " + shard + "] - " + message)
-    print(timeString + " - [mq-rcvr: " + shard + "] - " + message)
     
+    
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=rmq_host))
+    channel = connection.channel()
 
-# logit("worldHost: " + worldHost )
-# logit("worldPort: " + worldPort)
-# logit("worldHostRuby: " + worldHostRuby )
-# logit("worldPortRuby: " + worldPortRuby)
-# logit("rubyWorld: " + rubyWorld)
-# logit( "initialized")
+    channel.queue_declare(queue=queue_name)
 
-def generate_acct_num():
-    r = randrange(10000)
-    return str(r) 
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
 
-def RUBY_WORLD():
-    r = randrange(100)
-    if r < int(rubyWorld):
-        logit("world-ruby")
-        return worldHostRuby, worldPortRuby
-    else: 
-        logit("world-python")
-        return worldHost, worldPort
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-@app.route("/")
-def front_end():
-    httpStatus = 200
-    # logit("handling /")
-    res = ""
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
     try:
-        resH = requests.get('http://' + helloHost + ':5001' + "?account=" + generate_acct_num())
-        httpStatus = resH.status_code
-        res += "hello status: " + str(resH.status_code) + " - " + resH.text 
-    except Exception as e:
-        res += "hello status: " + repr(e)
-
-    try: 
-        lHost, lPort = RUBY_WORLD()
-        logit(lHost + ":" + lPort)
-        resW = requests.get('http://' + lHost + ':' + lPort)
-        httpStatus = resW.status_code
-        res += " | world status: " + str(resW.status_code) + " - " + resW.text 
-    except Exception as e:
-        httpStatus = 500
-        res += " world status: " + repr(e)
-
-    logit (res)
-    return res,  httpStatus
-
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
